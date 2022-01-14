be6de6f68191b95402139dcf6af903b876b00197
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-01-14 15:53:31 -0800

    added to file tree

---
+++ messages/messages.js
@@ -0,0 +1,15 @@
+import { checkAuth, logout } from '../fetch-utils.js';
+
+checkAuth();
+
+const logoutButton = document.getElementById('logout');
+
+logoutButton.addEventListener('click', () => {
+    logout();
+});
+
+window.addEventListener('resize', () => {
+    if (window.innerHeight > 565) {
+        window.scrollTo(0, 0);
+    }
+});
