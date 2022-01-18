313db32f49cabd579d02a898d932c463a7d1ee86
Ari Harlem-Caballero <ari.bash.harlem@gmail.com>
2022-01-18 10:00:04 -0800

    grab DOM elements for messages

--- messages/messages.js
+++ messages/messages.js
@@ -2,6 +2,11 @@ import { checkAuth, logout } from '../fetch-utils.js';
 
 checkAuth();
 
+const form = document.querySelector('form');
+const homeButton = document.querySelector('.home');
+const chatboxListEl = document.querySelector('.chatbox-list');
+const headEl = document.querySelector('.head');
+
 const logoutButton = document.getElementById('logout');
 
 logoutButton.addEventListener('click', () => {
